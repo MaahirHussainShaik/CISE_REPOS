f261797ec0718007d1c151f61786fc7d0da84a23
MaahirHussainShaik <142274442+MaahirHussainShaik@users.noreply.github.com>
2024-08-14 13:12:06 +1200

    Corrected the error by installing the package

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -10,17 +10,4 @@ describe('App Tests', () => {
     root.unmount();
   });
 
-  //Fail test
-  it('fails intentionally', () => {
-    const div = document.createElement('div');
-    const root = createRoot(div);
-    root.render(<App />);
-    
-    // This is an intentional failure to demonstrate a test that fails.
-    expect(div.textContent).toBe('This will fail');
-    
-    root.unmount();
-  });
-
-
 })
